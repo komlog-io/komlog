ca860c8bc3549d5db74b3b3cd6d5bf1793ab3027
juan cañete <jcazor@komlog.org>
2013-02-09 03:54:09 +0100

    komdc uses the new komimc module. Support for STORE_SAMPLE_MESSAGE added.

--- komdc/modules.py
+++ komdc/modules.py
@@ -10,12 +10,16 @@ from komcass import connection as casscon
 from komfs import api as fsapi
 from komapp import modules
 from komfig import komlogger
+from komimc import bus,messages
         
 class Validation(modules.Module):
     def __init__(self, config, instance_number):
         super(Validation,self).__init__(config, self.__class__.__name__, instance_number)
         self.watchdir = self.config.safe_get(sections.VALIDATION, options.SAMPLES_INPUT_PATH)
         self.outputdir = self.config.safe_get(sections.VALIDATION, options.SAMPLES_OUTPUT_PATH)
+        self.broker = self.config.safe_get(sections.VALIDATION, options.MESSAGE_BROKER)
+        if not self.broker:
+            self.broker = self.config.safe_get(sections.MAIN, options.MESSAGE_BROKER)
             
     def start(self):
         self.logger = komlogger.getLogger(self.config.conf_file, self.name)
@@ -24,7 +28,10 @@ class Validation(modules.Module):
             self.logger.error('Key '+options.SAMPLES_INPUT_PATH+' not found')
         elif not self.outputdir:
             self.logger.error('Key '+options.SAMPLES_OUTPUT_PATH+' not found')
+        elif not self.broker:
+            self.logger.error('Key '+options.MESSAGE_BROKER+' not found')
         else:
+            self.message_bus = bus.MessageBus(self.broker, self.name, self.instance_number, self.hostname, self.logger)
             self.__loop()
         
     def __loop(self):
@@ -42,7 +49,9 @@ class Validation(modules.Module):
                         fi = f[:-5]+'.qspl'
                         if self.validate(fi):
                             self.logger.debug('File validated successfully: '+f)
-                            os.rename(fi,os.path.join(self.outputdir,os.path.basename(fi)[:-5]+'.vspl'))
+                            fo = os.path.join(self.outputdir,os.path.basename(fi)[:-5]+'.vspl')
+                            os.rename(fi, fo)
+                            self.message_bus.sendMessage(messages.StoreSampleMessage(sample_file=fo))
                         else:
                             os.rename(fi,fi[:-5]+'.pspl')                                            
             else:
@@ -65,7 +74,10 @@ class Storing(modules.Module):
         self.sql_uri = self.config.safe_get(sections.STORING, options.SQL_URI)
         self.watchdir = self.config.safe_get(sections.STORING, options.SAMPLES_INPUT_PATH)
         self.outputdir = self.config.safe_get(sections.STORING, options.SAMPLES_OUTPUT_PATH)
- 
+        self.broker = self.config.safe_get(sections.STORING, options.MESSAGE_BROKER)
+        if not self.broker:
+            self.broker = self.config.safe_get(sections.MAIN, options.MESSAGE_BROKER)
+
     def start(self):
         self.logger = komlogger.getLogger(self.config.conf_file, self.name)
         self.logger.info('Storing module started')
@@ -77,33 +89,33 @@ class Storing(modules.Module):
             self.logger.error('Key '+options.SAMPLES_OUTPUT_PATH+' not found')
         elif not self.sql_uri:
             self.logger.error('Key '+options.SQL_URI+' not found')
+        elif not self.broker:
+            self.logger.error('Key '+options.MESSAGE_BROKER+' not found')
         else:
             self.cass_pool = casscon.Pool(keyspace=self.cass_keyspace, server_list=self.cass_servlist, pool_size=self.cass_poolsize)
             self.samples_cf = casscon.SamplesCF(self.cass_pool.connection_pool)
             self.sql_connection = dbcon.Connection(self.sql_uri)
+            self.message_bus = bus.MessageBus(self.broker, self.name, self.instance_number, self.hostname, self.logger)
             self.__loop()
         self.logger.info('Storing module exiting')
     
     def __loop(self):
         while True:
-            files = filter(os.path.isfile,glob.glob(os.path.join(self.watchdir,'*vspl')))
-            files.sort(key=lambda x: os.path.getmtime(x))
-            if len(files)>0:
-                for f in files:
-                    try:
-                        os.rename(f,f[:-5]+'.wspl')
-                    except OSError:
-                        #other instance took it firts
-                        self.logger.error('File already treated by other module instance: '+f)
-                    else:
-                        fi = f[:-5]+'.wspl'
-                        if self.store(fi):
-                            os.rename(fi,os.path.join(self.outputdir,os.path.basename(fi)[:-5]+'.sspl'))
-                        else:
-                            os.rename(fi,fi[:-5]+'.vspl')
-                                                
+            message = self.message_bus.retrieveMessage(messages.STORE_SAMPLE_MESSAGE)
+            f = message.sample_file
+            try:
+                os.rename(f,f[:-5]+'.wspl')
+            except OSError:
+            #other instance took it firts (it shouldn't because messages must be sent once)
+                self.logger.error('File already treated by other module instance: '+f)
             else:
-                time.sleep(5)
+                fi = f[:-5]+'.wspl'
+                if self.store(fi):
+                    fo = os.path.join(self.outputdir,os.path.basename(fi)[:-5]+'.sspl')
+                    os.rename(fi,fo)
+                else:
+                    fo = fi[:-5]+'.xspl'
+                    os.rename(fi,fo)
     
     def store(self, filename):
         self.logger.debug('Storing '+filename)
