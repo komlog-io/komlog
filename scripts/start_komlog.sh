2fab61bc518e23364a11075bcb995c6d6c32f819
juan cañete <jcazor@komlog.org>
2013-06-28 22:09:27 +0200

    Added start script and .gitignore file

---
+++ scripts/start_komlog.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+cd $HOME
+nohup python komlog/komai/komai.py > /var/log/komlog/komai.log 2>&1 &
+echo $! > /tmp/komai.pid
+nohup python komlog/komges/komges.py > /var/log/komlog/komges.log 2>&1 &
+echo $! > /tmp/komges.pid
+nohup python komlog/komdc/komdc.py > /var/log/komlog/komdc.log 2>&1 &
+echo $! > /tmp/komdc.pid
+nohup python komlog/komws2/komws2.py > /var/log/komlog/komws2.log 2>&1 &
+echo $! > /tmp/komws2.pid
+
