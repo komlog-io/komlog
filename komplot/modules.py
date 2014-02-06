6aeb077641d89f85d89ab8ae1bb864b9f4480754
juan cañete <jcazor@komlog.org>
2014-02-06 21:41:54 +0100

    Translucent legends :)

--- komplot/modules.py
+++ komplot/modules.py
@@ -125,7 +125,8 @@ class Plotter(modules.Module):
         plt.setp(ax.get_xticklabels(), fontsize=5)
         plt.setp(ax.get_yticklabels(), fontsize=6)
         if len(legends)<4:
-            plt.legend(p1,legends,loc=2,prop={'size':6})
+            leg=plt.legend(p1,legends,loc=2,prop={'size':6})
+            leg.get_frame().set_alpha(0.5)
         plt.gcf().autofmt_xdate()
         plt.savefig(os.path.join(self.params['plot_dest_dir'],str(gid)))
         msgresult.retcode=msgcodes.SUCCESS
