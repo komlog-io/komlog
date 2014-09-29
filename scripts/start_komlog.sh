c42719ad7d0d8a4ff50fdf7f016bf28c311892e2
juan cañete <jcazor@komlog.org>
2014-09-29 21:19:46 +0200

    Modified startup script

--- scripts/start_komlog.sh
+++ scripts/start_komlog.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 cd $HOME
-nohup python komlog/komplot/komplot.py > /var/log/komlog/komplot.log 2>&1 &
+nohup python komlogs/komplot/komplot.py > /var/log/komlog/komplot.log 2>&1 &
 echo $! > /tmp/komplot.pid
-nohup python komlog/komai/komai.py > /var/log/komlog/komai.log 2>&1 &
+nohup python komlogs/komai/komai.py > /var/log/komlog/komai.log 2>&1 &
 echo $! > /tmp/komai.pid
-nohup python komlog/komges/komges.py > /var/log/komlog/komges.log 2>&1 &
+nohup python komlogs/komges/komges.py > /var/log/komlog/komges.log 2>&1 &
 echo $! > /tmp/komges.pid
-nohup python komlog/komdc/komdc.py > /var/log/komlog/komdc.log 2>&1 &
+nohup python komlogs/komdc/komdc.py > /var/log/komlog/komdc.log 2>&1 &
 echo $! > /tmp/komdc.pid
-nohup python komlog/komrc/komrc.py > /var/log/komlog/komrc.log 2>&1 &
+nohup python komlogs/komrc/komrc.py > /var/log/komlog/komrc.log 2>&1 &
 echo $! > /tmp/komrc.pid
-nohup python komlog/komws2/komws2.py > /var/log/komlog/komws2.log 2>&1 &
+nohup python komlogs/komws2/komws2.py > /var/log/komlog/komws2.log 2>&1 &
 echo $! > /tmp/komws2.pid
 
