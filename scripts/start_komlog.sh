a277cc213fe857458c1f41f2fb271fb4a11bc97e
juan cañete <jcazor@komlog.org>
2014-11-23 18:35:23 +0100

    This happens if you dont test your changes

--- scripts/start_komlog.sh
+++ scripts/start_komlog.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cd $HOME
-nohup python3 bin/komai.py &
+nohup python3 komlogs/bin/komai.py &
 echo $! > /tmp/komai.pid
-nohup python3 bin/komges.py &
+nohup python3 komlogs/bin/komges.py &
 echo $! > /tmp/komges.pid
-nohup python3 bin/komdc.py &
+nohup python3 komlogs/bin/komdc.py &
 echo $! > /tmp/komdc.pid
-nohup python3 bin/komrc.py &
+nohup python3 komlogs/bin/komrc.py &
 echo $! > /tmp/komrc.pid
-nohup python3 bin/komweb.py &
+nohup python3 komlogs/bin/komweb.py &
 echo $! > /tmp/komweb.pid
 
