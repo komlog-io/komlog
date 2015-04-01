ccea6aba8bb008ed486bc3527e3d80b4aacb3a1f
juan cañete <jcazor@komlog.org>
2015-04-01 12:10:49 +0200

    Adapt code to version 2.5 of cassandra driver. Use time utilities provided in this version. Some minor changes to quotes too.

--- komcass/model/orm/quote.py
+++ komcass/model/orm/quote.py
@@ -6,47 +6,47 @@ Created on 01/10/2014
 '''
 
 class Quote(object):
-    def __init__(self, quotes=None):
-        self.quotes=quotes if quotes else {}
+    def __init__(self, quotes):
+        self.quotes=quotes
 
     def get_quote(self, quote):
-        if quote and quote in self.quotes:
+        if self.quotes and quote in self.quotes:
             return self.quotes[quote]
         else:
             return None
 
 class UserQuo(Quote):
-    def __init__(self,uid, quotes=None):
+    def __init__(self,uid, quotes):
         self.uid=uid
         super(UserQuo,self).__init__(quotes)
 
 class AgentQuo(Quote):
-    def __init__(self,aid, quotes=None):
+    def __init__(self,aid, quotes):
         self.aid=aid
         super(AgentQuo,self).__init__(quotes)
 
 class DatasourceQuo(Quote):
-    def __init__(self,did, quotes=None):
+    def __init__(self,did, quotes):
         self.did=did
         super(DatasourceQuo,self).__init__(quotes)
 
 class DatapointQuo(Quote):
-    def __init__(self,pid, quotes=None):
+    def __init__(self,pid, quotes):
         self.pid=pid
         super(DatapointQuo,self).__init__(quotes)
 
 class WidgetQuo(Quote):
-    def __init__(self,wid, quotes=None):
+    def __init__(self,wid, quotes):
         self.wid=wid
         super(WidgetQuo,self).__init__(quotes)
 
 class DashboardQuo(Quote):
-    def __init__(self,bid, quotes=None):
+    def __init__(self,bid, quotes):
         self.bid=bid
         super(DashboardQuo,self).__init__(quotes)
 
 class CircleQuo(Quote):
-    def __init__(self,cid, quotes=None):
+    def __init__(self,cid, quotes):
         self.cid=cid
         super(CircleQuo,self).__init__(quotes)
 
--- komlibs/general/time/timeuuid.py
+++ komlibs/general/time/timeuuid.py
@@ -1,12 +1,12 @@
 import uuid
 import time
-import random
+from cassandra import util
 
 node=0x010000000000
 str_node='010000000000'
 
 def get_unix_timestamp(u):
-    return int((u.time - 0x01B21DD213814000) / 10000000.0 * 1000)/1000.0
+    return util.unix_time_from_uuid1(u)
 
 def get_custom_sequence(u):
     return u.hex[0:20]
@@ -15,27 +15,8 @@ def get_uuid1_from_custom_sequence(sequence):
     return uuid.UUID(sequence+str_node)
 
 def uuid1(clock_seq=None, seconds=None):
-    '''Generate a UUID from a host ID, sequence number, and time.
-    If 'node' is not given, getnode() is used to obtain the hardware
-    address.  If 'clock_seq' is given, it is used as the sequence number;
-    otherwise a random 14-bit sequence number is chosen.
-    if seconds is given, is used to generate the timestamp of the UUID object,
-    otherwise, the current time is used '''
-    
     global node
     if not seconds:
         seconds=time.time()
-    nanoseconds=int(seconds * 1e9)
-    # 0x01b21dd213814000 is the number of 100-ns intervals between the
-    # UUID epoch 1582-10-15 00:00:00 and the Unix epoch 1970-01-01 00:00:00.
-    timestamp = int(nanoseconds/100) + 0x01b21dd213814000
-    if clock_seq is None:
-        clock_seq = random.randrange(1<<14) # instead of stable storage
-    time_low = timestamp & 0xffffffff
-    time_mid = (timestamp >> 32) & 0xffff
-    time_hi_version = (timestamp >> 48) & 0x0fff
-    clock_seq_low = clock_seq & 0xff
-    clock_seq_hi_variant = (clock_seq >> 8) & 0x3f
-    return uuid.UUID(fields=(time_low, time_mid, time_hi_version,
-                        clock_seq_hi_variant, clock_seq_low, node), version=1)
+    return util.uuid_from_time(time_arg=seconds, node=node, clock_seq=clock_seq)
 
